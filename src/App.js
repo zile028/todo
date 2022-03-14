c73e1686a0eb41a8f40492864cfd1fb8d4998b88
zile028 <zile028@gmail.com>
2022-03-15 00:34:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState([
+    {
+      task: "Uciti React",
+      done: false,
+    },
+    {
+      task: "Uciti NodeJs",
+      done: true,
+    },
+  ]);
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    setTask([...task, { task: newTask, done: false }]);
+    setNewTask("");
+  };
+
+  const removeTask = (el) => {
+    let taskCopy = [...task];
+    taskCopy.splice(el, 1);
+    setTask(taskCopy);
+  };
+
+  const changeStatus = (el) => {
+    let taskCopy = [...task];
+    taskCopy[el].done = !taskCopy[el].done;
+    setTask(taskCopy);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className="jumbotron text-center">
+        <h1>My Task</h1>
+        <div className="container">
+          <div className="row">
+            <input
+              type="text"
+              className="col-5 offset-3"
+              onChange={(e) => setNewTask(e.target.value)}
+              value={newTask}
+            />
+            <button className="btn btn-warning col-1" onClick={addTask}>
+              Add task
+            </button>
+          </div>
+        </div>
       </header>
-    </div>
+      <section className="container">
+        {task.map((el, index) => {
+          return (
+            <div className="card mb-3" key={index}>
+              <div className="card-header">{el.task}</div>
+              <div className="card-body">
+                {el.done ? "Urađen" : "Nije urađen"}
+              </div>
+              <div className="card-footer">
+                <button
+                  className="btn btn-danger"
+                  onClick={() => {
+                    removeTask(index);
+                  }}
+                >
+                  Remove
+                </button>
+                {el.done ? (
+                  <button
+                    className="btn btn-success"
+                    onClick={() => {
+                      changeStatus(index);
+                    }}
+                  >
+                    Otkaži
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-warning"
+                    onClick={() => {
+                      changeStatus(index);
+                    }}
+                  >
+                    Urađeno
+                  </button>
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </main>
   );
 }
 
---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="jumbotron text-center">
+      <h1>My Task</h1>
+    </header>
+  );
+}
+
+export default Header;
